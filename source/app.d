dfd90963a74ce427824e271a453d8ff026aa6a7f
Akihiro Shoji <alpha.kai.net@alpha-kai-net.info>
2020-06-14 00:39:38 +0900

    improve commandline option parsing

--- source/app.d
+++ source/app.d
@@ -2,6 +2,15 @@ import std.stdio;
 import std.format;
 import hvmd.opcode, hvmd.util, hvmd.deserialize, hvmd.init, hvmd.exec;
 
+enum CommandLineOptions {
+	JIT_Enable = "--jit-enable",
+	JIT_Disable = "--jit-disable",
+	Quiet = "--quiet",
+	Verbose = "--verbose",
+	Execute = "-x",
+	Dump = "-d"
+}
+
 void main(string[] args) {
 	if (args.length < 3) {
 		writefln("Usage: %s -x prog_path", args[0]);
@@ -16,19 +25,19 @@ void main(string[] args) {
 
 	foreach (arg; args) {
 		switch (arg) {
-		case "--jit-enable": {
+		case CommandLineOptions.JIT_Enable: {
 				JIT_ENABLED = true;
 				break;
 			}
-		case "--jit-disable": {
+		case CommandLineOptions.JIT_Disable: {
 				JIT_ENABLED = false;
 				break;
 			}
-		case "--quiet": {
+		case CommandLineOptions.Quiet: {
 				verbose = false;
 				break;
 			}
-		case "--verbose": {
+		case CommandLineOptions.Verbose: {
 				verbose = true;
 				break;
 			}
@@ -39,7 +48,7 @@ void main(string[] args) {
 	}
 
 	switch (targs[0]) {
-	case "-x": { //execute
+	case CommandLineOptions.Execute: { //execute
 			vm_init(JIT_ENABLED);
 
 			long[] compiled_file = load_compiled_file(targs[1]);
@@ -53,7 +62,7 @@ void main(string[] args) {
 			vm_exec(code);
 			break;
 		}
-	case "-d": { //dump
+	case CommandLineOptions.Dump: { //dump
 			vm_init(JIT_ENABLED);
 
 			long[] compiled_file = load_compiled_file(targs[1]);
